1de47d513b2cd34c50403bde83bdaf4b6195a961
trunghieuvn <hieu.trantrung1204@gmail.com>
2023-12-17 00:31:02 +0700

    Remove key

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,4 +1,4 @@
 // ignore_for_file: non_constant_identifier_names
 
 String BASE_URL = "https://api.openai.com/v1";
-String API_KEY = "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";
+String API_KEY = "{}";
